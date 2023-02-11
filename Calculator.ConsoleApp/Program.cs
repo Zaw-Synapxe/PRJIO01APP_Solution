916f77ba9601aeca10893bc26833abd25516344f
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2023-02-12 00:06:25 +0800

    Add project files.

---
+++ Calculator.ConsoleApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Calculator.ConloseApp
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the first number: ");
+                double? num1 = await ReadNumberAsync();
+                if (!num1.HasValue)
+                {
+                    Console.WriteLine("First number cannot be null");
+                    continue;
+                }
+
+                Console.WriteLine("Enter the second number: ");
+                double? num2 = await ReadNumberAsync();
+                if (!num2.HasValue)
+                {
+                    Console.WriteLine("Second number cannot be null");
+                    continue;
+                }
+
+                Console.WriteLine("Enter the operator (+, -, *, /, %): ");
+                string op = Console.ReadLine();
+
+                double result;
+                switch (op)
+                {
+                    case "+":
+                        result = num1.Value + num2.Value;
+                        break;
+                    case "-":
+                        result = num1.Value - num2.Value;
+                        break;
+                    case "*":
+                        result = num1.Value * num2.Value;
+                        break;
+                    case "/":
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Cannot divide by zero");
+                            continue;
+                        }
+                        result = num1.Value / num2.Value;
+                        break;
+                    case "%":
+                        result = num1.Value % num2.Value;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator");
+                        continue;
+                }
+
+                Console.WriteLine("Result: " + result);
+
+                Console.WriteLine("Do you want to continue (y/n)? ");
+                if (Console.ReadLine().ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+
+        static async System.Threading.Tasks.Task<double?> ReadNumberAsync()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    return null;
+                }
+
+                try
+                {
+                    return Convert.ToDouble(input);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number. Please try again: ");
+                }
+            }
+        }
+    }
+}
---
+++ ChatGPT.APIClient.ConsoleApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+// API key (https://platform.openai.com/account/api-keys)
+// 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
+// https://gpttools.com/comparisontool
+// https://jsoneditoronline.org
+
+
+class Program
+{
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+
+       
+        args = new string[1];
+        args[0] = "Say Hellow from ChatGPT";
+
+        Console.WriteLine("First argument: {0}", args[0]);
+        
+
+        if (args.Length > 0)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
+
+            var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+                Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+            /*
+            {"id":"cmpl-6gVkMRM8WCUNq4rHCYqST7HqPce7O","object":"text_completion","created":1675589830,"model":"text-davinci-001","choices":[{"text":"\n\nHello! Thank you for considering ChatGPT. We are a rapidly growing company that provides a unique and fun way to earn money online by completing simple tasks. Our platform is simple to use and our community is friendly and helpful. We hope you will consider joining us and helping us grow!","index":0,"logprobs":null,"finish_reason":"stop"}],"usage":{"prompt_tokens":7,"completion_tokens":60,"total_tokens":67}}
+            */
+
+            Console.WriteLine(responseString);
+
+            try
+            {
+                //string json = "{\"name\":\"John\",\"age\":30,\"city\":\"New York\"}";
+                //dynamic obj = JsonConvert.DeserializeObject<dynamic>(json);
+
+                //Console.WriteLine("Name: " + obj.name);
+                //Console.WriteLine("Age: " + obj.age);
+                //Console.WriteLine("City: " + obj.city);
+
+
+                var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                string guess = GuessCommand(dyData!.choices[0].text);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"---> My guess at the command prompt is: {guess}");
+                Console.ResetColor();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"---> Could not deserialize the JSON: {ex.Message}");
+            }
+
+            
+        }
+        else
+        {
+            Console.WriteLine("---> You need to provide some input");
+        }
+
+
+    }
+
+    static string GuessCommand(string raw)
+    {
+        Console.WriteLine("---> GPT-3 API Returned Text:");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(raw);
+
+        var lastIndex = raw.LastIndexOf('\n');
+
+        string guess = raw.Substring(lastIndex + 1);
+
+        Console.ResetColor();
+
+        TextCopy.ClipboardService.SetText(guess);
+
+        return guess;
+    }
+
+}
+
+
