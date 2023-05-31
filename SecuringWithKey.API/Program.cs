fe881f2c08a66245a35a2c81542ba3f75a2f4669
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2023-06-01 00:57:25 +0800

    commit 20230601

---
+++ SecuringWithKey.API/Program.cs
@@ -0,0 +1,43 @@
+using SecuringWithKey.API.Middleware;
+using SecuringWithKey.API.Sinks;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.CustomSink()
+        .Enrich.FromLogContext()
+        .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseMiddleware<ApiKeyMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware(typeof(ExceptionHandlingMiddleware));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SecuringWithKey.API/Sinks/CustomSinkExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Configuration;
+using Serilog;
+
+namespace SecuringWithKey.API.Sinks
+{
+    public static class CustomSinkExtensions
+    {
+        public static LoggerConfiguration CustomSink(this LoggerSinkConfiguration loggerConfiguration)
+        {
+            return loggerConfiguration.Sink(new CustomSink());
+        }
+    }
+}
