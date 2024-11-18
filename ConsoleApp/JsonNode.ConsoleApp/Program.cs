b5eee1ee8004e9f04649328654628b11f8ed275a
zwmyint <zwmyint@gmail.com>
2024-11-18 17:24:52 +0800

    commit 20241118 1724

---
+++ ConsoleApp/JsonNode.ConsoleApp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace JsonNodeXYZ.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 
+            Console.WriteLine("... Program Started ...");
+            //Console.WriteLine($"{args[0]} {args[1]}");
+
+            ////// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/use-dom
+            
+            #region "Use JsonNode"
+            ////// ------------------------------------------------
+            ////string jsonString = """
+            ////{
+            ////  "Date": "2024-08-01T00:00:00",
+            ////  "Temperature": 25,
+            ////  "Summary": "Hot",
+            ////  "DatesAvailable": [
+            ////    "2024-08-01T00:00:00",
+            ////    "2024-08-02T00:00:00"
+            ////  ],
+            ////  "TemperatureRanges": {
+            ////      "Cold": {
+            ////          "High": 20,
+            ////          "Low": -10
+            ////      },
+            ////      "Hot": {
+            ////          "High": 60,
+            ////          "Low": 20
+            ////      }
+            ////  }
+            ////}
+            ////""";
+
+            ////// Create a JsonNode DOM from a JSON string.
+            ////JsonNode forecastNode = JsonNode.Parse(jsonString)!;
+
+            ////// Write JSON from a JsonNode
+            ////var options = new JsonSerializerOptions { WriteIndented = true };
+            ////Console.WriteLine(forecastNode!.ToJsonString(options));
+
+            ////// Get value from a JsonNode.
+            ////JsonNode temperatureNode = forecastNode!["Temperature"]!;
+            ////Console.WriteLine($"Type={temperatureNode.GetType()}");
+            ////Console.WriteLine($"JSON={temperatureNode.ToJsonString()}");
+            ////// Get a typed value from a JsonNode.
+            ////int temperatureInt = (int)forecastNode!["Temperature"]!;
+            ////Console.WriteLine($"Value={temperatureInt}");
+            ////// Get a typed value from a JsonNode by using GetValue<T>.
+            ////temperatureInt = forecastNode!["Temperature"]!.GetValue<int>();
+            ////Console.WriteLine($"TemperatureInt={temperatureInt}");
+            ////// Get a JSON object from a JsonNode.
+            ////JsonNode temperatureRanges = forecastNode!["TemperatureRanges"]!;
+            ////Console.WriteLine($"Type={temperatureRanges.GetType()}");
+            ////Console.WriteLine($"JSON={temperatureRanges.ToJsonString()}");
+            ////// Get a JSON array from a JsonNode.
+            ////JsonNode datesAvailable = forecastNode!["DatesAvailable"]!;
+            ////Console.WriteLine($"Type={datesAvailable.GetType()}");
+            ////Console.WriteLine($"JSON={datesAvailable.ToJsonString()}");
+            ////// Get an array element value from a JsonArray.
+            ////JsonNode firstDateAvailable = datesAvailable[0]!;
+            ////Console.WriteLine($"Type={firstDateAvailable.GetType()}");
+            ////Console.WriteLine($"JSON={firstDateAvailable.ToJsonString()}");
+            ////// Get a typed value by chaining references.
+            ////int coldHighTemperature = (int)forecastNode["TemperatureRanges"]!["Cold"]!["High"]!;
+            ////Console.WriteLine($"TemperatureRanges.Cold.High={coldHighTemperature}");
+            ////// Parse a JSON array
+            ////var datesNode = JsonNode.Parse(@"[""2024-08-01T00:00:00"",""2024-08-02T00:00:00""]");
+            ////JsonNode firstDate = datesNode![0]!.GetValue<DateTime>();
+            ////Console.WriteLine($"firstDate={firstDate}");
+            ////// ------------------------------------------------
+            #endregion
+
+            #region "Create a JsonNode DOM with object"
+            ////// ------------------------------------------------
+            ////// Create a new JsonObject using object initializers.
+            ////var forecastObject = new JsonObject
+            ////{
+            ////    ["Date"] = new DateTime(2024, 10, 29),
+            ////    ["Temperature"] = 25,
+            ////    ["Summary"] = "Hot",
+            ////    ["DatesAvailable"] = new JsonArray(
+            ////        new DateTime(2024, 11, 1), new DateTime(2024, 11, 2)),
+            ////    ["TemperatureRanges"] = new JsonObject
+            ////    {
+            ////        ["Cold"] = new JsonObject
+            ////        {
+            ////            ["High"] = 20,
+            ////            ["Low"] = -10
+            ////        }
+            ////    },
+            ////    ["SummaryWords"] = new JsonArray("Cool", "Windy", "Humid")
+            ////};
+
+            ////// Add an object.
+            ////forecastObject!["TemperatureRanges"]!["Hot"] =
+            ////    new JsonObject { ["High"] = 60, ["Low"] = 20 };
+
+            ////// Remove a property.
+            ////////forecastObject.Remove("SummaryWords");
+
+            ////// Change the value of a property.
+            ////forecastObject["Date"] = new DateTime(2024, 11, 1);
+
+            ////var options = new JsonSerializerOptions { WriteIndented = true };
+            ////Console.WriteLine(forecastObject.ToJsonString(options));
+            ////// ------------------------------------------------
+            #endregion
+
+
+
+
+            await Task.Delay(500);
+            Console.WriteLine("... Program End ...");
+            //
+        }
+    }
+}
---
+++ ConsoleApp/SQLServer1.ConsoleApp/Program.cs
@@ -0,0 +1,144 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace SQLServer1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Connect to SQL Server and demo Create, Read, Update and Delete operations.");
+
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "(local)";   // update me
+                //builder.DataSource = "ZAW-MAXIMUS-XI";   // update me
+                builder.UserID = "sa";              // update me
+                builder.Password = "$A12312312Zaw";      // update me
+                builder.InitialCatalog = "Northwind";
+
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+
+                    connection.Open();
+                    Console.WriteLine("Done. Connection Connected !");
+
+                    String sql = "";
+                    StringBuilder sb = new StringBuilder();
+
+                    //////// Create a sample database
+                    //////Console.Write("Dropping and creating database 'Northwind' ... ");
+                    //////sql = "DROP DATABASE IF EXISTS [SampleDB]; CREATE DATABASE [Northwind]";
+                    //////using (SqlCommand command = new SqlCommand(sql, connection))
+                    //////{
+                    //////    command.ExecuteNonQuery();
+                    //////    Console.WriteLine("Done.");
+                    //////}
+
+                    //////// Create a Table and insert some sample data
+                    //////Console.Write("Creating sample table with data, press any key to continue...");
+                    //////Console.ReadKey(true);
+
+                    //////sb.Append("USE Northwind; ");
+                    //////sb.Append("CREATE TABLE Employees1 ( ");
+                    //////sb.Append(" Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+                    //////sb.Append(" Name NVARCHAR(50), ");
+                    //////sb.Append(" Location NVARCHAR(50) ");
+                    //////sb.Append("); ");
+                    //////sb.Append("INSERT INTO Employees1 (Name, Location) VALUES ");
+                    //////sb.Append("(N'Jared', N'Australia'), ");
+                    //////sb.Append("(N'Nikita', N'India'), ");
+                    //////sb.Append("(N'Tom', N'Germany'); ");
+                    //////sql = sb.ToString();
+                    //////using (SqlCommand command = new SqlCommand(sql, connection))
+                    //////{
+                    //////    command.ExecuteNonQuery();
+                    //////    Console.WriteLine("Done.");
+                    //////}
+
+                    //////// INSERT demo
+                    //////Console.Write("Inserting a new row into table, press any key to continue...");
+                    //////Console.ReadKey(true);
+                    //////sb.Clear();
+                    //////sb.Append("INSERT Employees1 (Name, Location) ");
+                    //////sb.Append("VALUES (@name, @location);");
+                    //////sql = sb.ToString();
+                    //////using (SqlCommand command = new SqlCommand(sql, connection))
+                    //////{
+                    //////    command.Parameters.AddWithValue("@name", "Jake");
+                    //////    command.Parameters.AddWithValue("@location", "United States");
+                    //////    int rowsAffected = command.ExecuteNonQuery();
+                    //////    Console.WriteLine(rowsAffected + " row(s) inserted");
+                    //////}
+
+                    //////// UPDATE demo
+                    //////String userToUpdate = "Nikita";
+                    //////Console.Write("Updating 'Location' for user '" + userToUpdate + "', press any key to continue...");
+                    //////Console.ReadKey(true);
+                    //////sb.Clear();
+                    //////sb.Append("UPDATE Employees1 SET Location = N'United States' WHERE Name = @name");
+                    //////sql = sb.ToString();
+                    //////using (SqlCommand command = new SqlCommand(sql, connection))
+                    //////{
+                    //////    command.Parameters.AddWithValue("@name", userToUpdate);
+                    //////    int rowsAffected = command.ExecuteNonQuery();
+                    //////    Console.WriteLine(rowsAffected + " row(s) updated");
+                    //////}
+
+                    //////// DELETE demo
+                    //////String userToDelete = "Jared";
+                    //////Console.Write("Deleting user '" + userToDelete + "', press any key to continue...");
+                    //////Console.ReadKey(true);
+                    //////sb.Clear();
+                    //////sb.Append("DELETE FROM Employees1 WHERE Name = @name;");
+                    //////sql = sb.ToString();
+                    //////using (SqlCommand command = new SqlCommand(sql, connection))
+                    //////{
+                    //////    command.Parameters.AddWithValue("@name", userToDelete);
+                    //////    int rowsAffected = command.ExecuteNonQuery();
+                    //////    Console.WriteLine(rowsAffected + " row(s) deleted");
+                    //////}
+
+
+                    // READ demo
+                    Console.WriteLine("Reading data from table, press any key to continue...");
+                    Console.ReadKey(true);
+                    sql = "SELECT Id, Name, Location FROM Employees1;";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                            }
+                        }
+                    }
+
+                    //
+                }
+
+
+                //
+            }
+            catch (SqlException ex) {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
+        }
+
+        //
+    }
+}
