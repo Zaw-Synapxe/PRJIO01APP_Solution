11f9b88e0edbabc1e54359baf35bba96642f462f
zwmyint <zwmyint@gmail.com>
2024-12-04 21:14:12 +0800

    commit-20241204

--- ConsoleApp/JsonNode.ConsoleApp/Program.cs
+++ ConsoleApp/JsonNode.ConsoleApp/Program.cs
@@ -5,6 +5,8 @@ namespace JsonNodeXYZ.ConsoleApp
 {
     class Program
     {
+        public static DateTime[]? DatesAvailable { get; set; }
+
         static async Task Main(string[] args)
         {
             // 
@@ -12,7 +14,7 @@ namespace JsonNodeXYZ.ConsoleApp
             //Console.WriteLine($"{args[0]} {args[1]}");
 
             ////// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/use-dom
-            
+
             #region "Use JsonNode"
             ////// ------------------------------------------------
             ////string jsonString = """
@@ -112,12 +114,154 @@ namespace JsonNodeXYZ.ConsoleApp
             ////// ------------------------------------------------
             #endregion
 
+            #region "Deserialize subsections of a JSON payload"
+            ////// ------------------------------------------------
+            ////string jsonString = """
+            ////{
+            ////  "Date": "2024-11-01T00:00:00",
+            ////  "Temperature": 25,
+            ////  "Summary": "Hot",
+            ////  "DatesAvailable": [
+            ////    "2024-11-01T00:00:00",
+            ////    "2024-11-02T00:00:00"
+            ////  ],
+            ////  "TemperatureRanges": {
+            ////      "Cold": {
+            ////          "High": 20,
+            ////          "Low": -10
+            ////      },
+            ////      "Hot": {
+            ////          "High": 60,
+            ////          "Low": 20
+            ////      }
+            ////  }
+            ////}
+            ////""";
+
+            ////// Parse all of the JSON.
+            ////JsonNode forecastNode = JsonNode.Parse(jsonString)!;
+
+            ////// Get a single value
+            ////int hotHigh = forecastNode["TemperatureRanges"]!["Hot"]!["High"]!.GetValue<int>();
+            ////Console.WriteLine($"Hot.High={hotHigh}");
+            ////// Get a subsection and deserialize it into a custom type.
+            ////JsonObject temperatureRangesObject = forecastNode!["TemperatureRanges"]!.AsObject();
+            ////using var stream = new MemoryStream();
+            ////using var writer = new Utf8JsonWriter(stream);
+            ////temperatureRangesObject.WriteTo(writer);
+            ////writer.Flush();
+            ////TemperatureRanges? temperatureRanges =
+            ////    JsonSerializer.Deserialize<TemperatureRanges>(stream.ToArray());
+            ////Console.WriteLine($"Cold.Low={temperatureRanges!["Cold"].Low}, Hot.High={temperatureRanges["Hot"].High}");
+            ////// Get a subsection and deserialize it into an array.
+            ////JsonArray datesAvailable = forecastNode!["DatesAvailable"]!.AsArray()!;
+            ////Console.WriteLine($"DatesAvailable[0]={datesAvailable[0]}");
+            ////// ------------------------------------------------
+            #endregion
+
+            #region "JsonNode average grade example"
+            ////// ------------------------------------------------
+            ////string jsonString = """
+            ////{
+            ////  "Class Name": "Science",
+            ////  "Teacher\u0027s Name": "Jane",
+            ////  "Semester": "2019-01-01",
+            ////  "Students": [
+            ////    {
+            ////      "Name": "John",
+            ////      "Grade": 94.3
+            ////    },
+            ////    {
+            ////      "Name": "James",
+            ////      "Grade": 81.0
+            ////    },
+            ////    {
+            ////      "Name": "Julia",
+            ////      "Grade": 91.9
+            ////    },
+            ////    {
+            ////      "Name": "Jessica",
+            ////      "Grade": 72.4
+            ////    },
+            ////    {
+            ////      "Name": "Johnathan"
+            ////    }
+            ////  ],
+            ////  "Final": true
+            ////}
+            ////""";
+            ////double sum = 0;
+            ////JsonNode document = JsonNode.Parse(jsonString)!;
+
+            ////JsonNode root = document.Root;
+            ////JsonArray studentsArray = root["Students"]!.AsArray();
+
+            ////int count = studentsArray.Count;
+            ////foreach (JsonNode? student in studentsArray)
+            ////{
+            ////    if (student?["Grade"] is JsonNode gradeNode)
+            ////    {
+            ////        sum += (double)gradeNode;
+            ////    }
+            ////    else
+            ////    {
+            ////        sum += 70;
+            ////    }
+            ////}
+
+            ////double average = sum / count;
+            ////Console.WriteLine($"Average grade : {average}");
+            ////// ------------------------------------------------
+            #endregion
+
+            #region "JsonNode with JsonSerializerOptions"
+            ////// ------------------------------------------------
+            Person person = new() { Name = "Nancy" };
+
+            // Default serialization - Address property included with null token.
+            // Output: {"Name":"Nancy","Address":null}
+            string personJsonWithNull = JsonSerializer.Serialize(person);
+            Console.WriteLine(personJsonWithNull);
 
 
+
+            ////// ------------------------------------------------
+            #endregion
+
+            #region "333"
+            ////// ------------------------------------------------
+
+            ////// ------------------------------------------------
+            #endregion
+
+            #region "444"
+            ////// ------------------------------------------------
+
+            ////// ------------------------------------------------
+            #endregion
 
             await Task.Delay(500);
             Console.WriteLine("... Program End ...");
             //
         }
     }
+
+    public class TemperatureRanges : Dictionary<string, HighLowTemps>
+    {
+    }
+
+    public class HighLowTemps
+    {
+        public int High { get; set; }
+        public int Low { get; set; }
+    }
+
+    public class Person
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+
+
+    //
 }
